5cf5df8ed8228bd557ef980dbcd7ae68e969485c
Akshay V.R <64145832+akshayvr-programmer@users.noreply.github.com>
2021-05-25 11:47:05 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int x;
+int y;
+
+int getHCF() {
+while (x != y) {
+
+    if (x > y) {
+
+        x -= y;
+
+    } else {
+        x -= y;
+
+
+    }
+    return abs(x);
+
+
+}
+
+}
 
 int main()
 {
-    int x, y;
-
-    cout << "Enter two numbers: ";
     cin >> x >> y;
-    
-    while(x != y)
-    {
-        if(x > y)
-            x -= y;
-        else
-            x -= y;
-    }
+    cout << "HCF =" << getHCF();
 
-    cout << "HCF = " << x;
     return 0;
 }
